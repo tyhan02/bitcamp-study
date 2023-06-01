7d649a47094c5666d1758f5bc33bf6b1d50dbbad
Taeyoung Han <tyhan02@naver.com>
2023-06-01 10:19:17 +0900

    ..

--- java-lang/app/src/test/java/bitcamp/AppTest.java
+++ java-lang/app/src/test/java/bitcamp/AppTest.java
@@ -10,5 +10,6 @@ class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+
     }
 }
